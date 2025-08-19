ab169e34505b5ae86d3eda868829592639f01ea7
1eeHyun <dlee2006@uic.edu>
2025-08-18 19:26:29 -0500

    added domain with A and CNAME records

--- src/main/java/com/ldh/portfolio/config/CorsConfig.java
+++ src/main/java/com/ldh/portfolio/config/CorsConfig.java
@@ -5,17 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${app.cors.allowed-origin}")
-    private String allowedOrigin;
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins(allowedOrigin)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+
+        String[] origins = Arrays.stream(allowedOrigins.split(","))
+                .map(String::trim)
+                .toArray(String[]::new);
+
+        registry.addMapping("/**")
+                .allowedOrigins(origins)
+                .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
